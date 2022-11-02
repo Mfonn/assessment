9701d8d9be50fbd070527d1019dd680e194402cc
Mfon <mfonnta05@gmail.com>
2022-11-02 16:46:59 +0100

    new tests

--- APIapp/tests.py
+++ APIapp/tests.py
@@ -1,10 +1,2 @@
 from django.test import TestCase
-from  models.py import  Books
-
-class BooksTest(TestCase):
-	def testBooks(self):
-		post = Post(title="Harry Porter", author="J.K Rowling")
-		self.assertEqual(post.title, "Harry Porter")
-		self.assertEqual(post.author, "J.K Rowling")
-
 # Create your tests here.
