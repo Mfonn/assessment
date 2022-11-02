275edf4216460cbe94b695e83ced2f411f65153b
Mfon <mfonnta05@gmail.com>
2022-11-02 16:42:16 +0100

    adjusted unit test

--- APIapp/tests.py
+++ APIapp/tests.py
@@ -3,9 +3,8 @@ from  models.py import  Books
 
 class BooksTest(TestCase):
 	def testBooks(self):
-		post = Post(title="Harry Porter", author="J.K Rowling", status="True")
+		post = Post(title="Harry Porter", author="J.K Rowling")
 		self.assertEqual(post.title, "Harry Porter")
 		self.assertEqual(post.author, "J.K Rowling")
-		self.assertEqual(post.status, "True")
 
 # Create your tests here.
