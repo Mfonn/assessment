ea63cd76e88c8a0fc498f173e8fc55868c7cb61b
Mfon <mfonnta05@gmail.com>
2022-11-02 16:36:35 +0100

    add unit test and static files

--- APIapp/tests.py
+++ APIapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from  models.py import  Books
+
+class BooksTest(TestCase):
+	def testBooks(self):
+		post = Post(title="Harry Porter", author="J.K Rowling", status="True")
+		self.assertEqual(post.title, "Harry Porter")
+		self.assertEqual(post.author, "J.K Rowling")
+		self.assertEqual(post.status, "True")
 
 # Create your tests here.
